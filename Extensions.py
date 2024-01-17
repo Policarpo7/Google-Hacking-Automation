fef4c8fec360d4b9601ada065741029f5a1e9881
Gustavo Policarpo <104924112+Policarpo7@users.noreply.github.com>
2024-01-17 15:04:54 -0300

    Update Extensions.py

--- Extensions.py
+++ Extensions.py
@@ -3,12 +3,11 @@ import requests
 from urllib.parse import urlencode
 from bs4 import BeautifulSoup
 
-
 def show_banner():
-  print("=" * 50)
-  print("  GOOGLE HACKING AUTOMATION")
-  print("  Created by Gustavo Policarpo")
-  print("=" * 50)
+    print("=" * 50)
+    print("  GOOGLE HACKING AUTOMATION")
+    print("  Created by Gustavo Policarpo")
+    print("=" * 50)
 
 show_banner()
 
@@ -17,42 +16,42 @@ google_hacking_operators = ['inurl:', 'intext:', 'intitle:', 'site:', 'cache:']
 
 # Função para realizar buscas no Google
 def google_search(query):
-  headers = {'User-Agent': 'Mozilla/5.0'}
-  params = {'q': query}
-  response = requests.get(f"https://www.google.com/search?{urlencode(params)}",
-                          headers=headers)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {'q': query}
+    response = requests.get(f"https://www.google.com/search?{urlencode(params)}", headers=headers)
 
-  if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    links = []
-    for a in soup.select('a[href^="/url?"]'):
-      href = a['href']
-      if href.startswith('/url?'):
-        link = href.split('&')[0]
-        link = link.split('=')[1]
-        links.append(requests.utils.unquote(link))
-    return links
-  else:
-    print(f"Erro {response.status_code} na requisição de: {query}")
-    return []
-
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        links = []
+        for a in soup.select('a[href^="/url?"]'):
+            href = a['href']
+            if href.startswith('/url?'):
+                link = href.split('&')[0]
+                link = link.split('=')[1]
+                decoded_link = requests.utils.unquote(link)
+                # Filtra links indesejados
+                if all(x not in decoded_link for x in ['maps.google.com', 'support.google.com', 'accounts.google.com']):
+                    links.append(decoded_link)
+        return links
+    else:
+        print(f"Erro {response.status_code} na requisição de: {query}")
+        return []
 
 # Função para imprimir os resultados de maneira organizada
 def print_results(dork, links):
-  print(f"Resultados para {dork}:")
-  if links:
-    for link in links:
-      print(link)
-  else:
-    print("Nenhum link encontrado.")
-  print("-" * 80)
-
+    print(f"Resultados para {dork}:")
+    if links:
+        for link in links:
+            print(link)
+    else:
+        print("Nenhum link encontrado.")
+    print("-" * 80)
 
 # Solicita entrada do usuário para termo de busca
 search_term = input("Digite o termo que você deseja pesquisar: ").strip()
 
 # Executa a busca para cada operador de pesquisa
 for operator in google_hacking_operators:
-  dork = f"{operator}\"{search_term}\""
-  links = google_search(dork)
-  print_results(dork, links)
+    dork = f"{operator}\"{search_term}\""
+    links = google_search(dork)
+    print_results(dork, links)
